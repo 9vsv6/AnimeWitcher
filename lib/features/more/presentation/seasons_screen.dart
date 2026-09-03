5867b860fb386f8c54a271528d06b346b799d0c8
Fares669 <108722956+Fares669@users.noreply.github.com>
2026-09-03 18:34:09 +0300

    feat: extend right-click refresh to supported pages

--- lib/features/more/presentation/seasons_screen.dart
+++ lib/features/more/presentation/seasons_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:animewitcher/shared/widgets/secondary_mouse_refresh_indicator.dart';
 import 'package:animewitcher/shared/widgets/apple_liquid_glass.dart';
 import 'package:animewitcher/shared/widgets/underline_segment_tabs.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -159,7 +160,7 @@ class _SeasonsScreenState extends ConsumerState<SeasonsScreen>
                       LazyTabChild(
                         controller: _tabController,
                         index: index,
-                        builder: (context) => RefreshIndicator(
+                        builder: (context) => SecondaryMouseRefreshIndicator(
                           onRefresh: _refreshSeasons,
                           child: _tabBody(data, isArabic, index),
                         ),
@@ -711,3 +712,4 @@ class _LoadError extends StatelessWidget {
     );
   }
 }
+
